7d22159fa894f6e7092d0b24d0cf237f99268681
Nikita Skriaha <nikitaskraga6@gmail.com>
2024-11-14 19:24:57 +0200

    react-typescrip-app-resetupt

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './pages/Login'
-import Login from './pages/Login';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Login/>
-);
